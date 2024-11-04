16c79400dc64b83412313976a9ec5e01a2ff9762
Gsszs <gabriel.selvenca@icloud.com>
2024-11-04 17:06:33 -0300

    Ex 5

---
+++ Assets/Scripts/Ex05.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+/*
+(Pontuação de missão) Após completar uma missão, o jogador
+recebe pontos. Verifique se ele ganhou mais de 50 pontos. Se sim,
+exiba "Missão bem-sucedida"; caso contrário, "Missão
+incompleta".
+*/
+
+public class Ex5 : MonoBehaviour
+{
+
+    [SerializeField] int pontosJogador = 30;
+    void Start()
+    {
+        print(pontosJogador >= 50 ? "Missão bem-sucedida" : "Missão incompleta");
+    }
+
+    void Update()
+    {
+        
+    }
+}
