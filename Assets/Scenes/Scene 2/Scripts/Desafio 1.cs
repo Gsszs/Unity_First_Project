95f61e47c13c1624cb382a369544643afb982f4a
Gsszs <gabriel.selvenca@icloud.com>
2024-11-11 15:43:26 -0300

    Desafio 1

---
+++ Assets/Scenes/Scene 2/Scripts/Desafio 1.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Linq;
+using UnityEngine;
+
+// Escreva um programa que conta o número de consoantes em uma string.
+
+public class Desafio1 : MonoBehaviour
+{
+
+    [SerializeField] char[] vogais = { 'a', 'e', 'i', 'o', 'u'};
+    void Start()
+    {
+        string frase = "abcdefg";
+        int consoantes = 0;
+        foreach(char c in frase.ToLower())
+        {
+            if (Char.IsDigit(c) || vogais.Contains(c) || !Char.IsLetter(c)) { continue; }
+            consoantes++;
+        }
+        print($"A frase \"{frase}\" tem {consoantes} consoantes.");
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
